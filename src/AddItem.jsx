1d28bf4f3787411078972f832a97b40726397a8c
Fathiu <bjmustapha48@gmail.com>
2026-04-09 15:48:20 +0100

    .

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -20,7 +20,6 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
       <button
       type='submit'
       aria-label='Add Item'
-    //   onClick={()=>inputRef.current.focus()}
       >
        <FaPlus />
       </button>
